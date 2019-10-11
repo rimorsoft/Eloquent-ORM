8f5cab5f559166d74e578be8b219835be9af7c22
italomoralesf <i@italomoralesf.com>
2019-10-10 19:56:38 -0500

    proyecto de relaciones en Laravel - Eloquent ORM

---
+++ database/migrations/2019_08_10_215700_create_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->string('instagram')->nullable();
+            $table->string('github')->nullable();
+            $table->string('web')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
